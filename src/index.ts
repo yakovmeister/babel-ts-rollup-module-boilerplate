f2dbc771b3f8c425ee4e166a680983af9625ee4d
yakovmeister <so@tfwno.gf>
2018-06-26 10:28:04 +0800

    removed babel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-import '@babel/polyfill'
-
 /**
  * your app starts here.
  */
 const Hello = 'world'
+
+function x(aw: string) {
+  return aw + y()
+}
+
+
+function y() {
+  const arr = [1, 2, 3]
+
+  return [].concat(...arr)
+}
+
+export default x
